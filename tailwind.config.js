750dca5db03acc4b280aec07736122931bd60325
Kryštof Klaban <krystof.klaban@puxdesign.cz>
2023-11-21 18:06:30 +0100

    Add tailwind preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import themeConfig from "./lib/tailwind"
+
+console.log("dfdfdfdf   ", themeConfig())
+
 export default {
+  presets: [themeConfig()],
   jit: true,
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
     './lib/**/*.{js,ts,jsx,tsx}',
     './node_modules/vite-component-library-test/**/*.js',
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  corePlugins: {
-    preflight: false,
-  },
 }
