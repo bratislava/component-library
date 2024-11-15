1ddd95b83c4af2a92db846966a3bf629d81bdd9d
PavelFilo <pvlfilo@gmail.com>
2024-11-15 12:20:02 +0100

    changes based on comments

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
   semi: false,
   singleQuote: true,
   printWidth: 100,
-  tailwindFunctions: ['cn', 'twMerge', 'clsx'],
+  // These libraries for handling classes are not yet used. But i will leave this rule here for future development
+  tailwindFunctions: ['cx', 'classnames', 'clsx', 'cn', 'twMerge', 'tw'],
   tailwindConfig: './tailwind.config.js',
 }
