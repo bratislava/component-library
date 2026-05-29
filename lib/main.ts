83916dcd872a4a8f4c3596d37f691df8699dcffc
Ty-ci <teachy.jaho@gmail.com>
2026-05-29 14:42:45 +0200

    Export normalizeSkTexts, set exact versions

--- lib/main.ts
+++ lib/main.ts
@@ -1,14 +1,18 @@
 export {
-  default as Button,
-  type ButtonAnchorProps,
-  type ButtonButtonProps,
-  type ButtonProps,
-} from './components/Button/Button'
-export {
   ComponentLibraryProvider,
   type ComponentLibraryProviderProps,
   type LinkComponentProps,
   useComponentLibraryContext,
 } from './components/providers/ComponentLibraryProvider'
-export { default as Spinner } from './components/Spinner/Spinner'
+
+export { normalizeSkText } from './components/Typography/utils/normalizeSkText'
 export { default as Typography, type TypographyProps } from './components/Typography/Typography'
+
+export {
+  default as Button,
+  type ButtonAnchorProps,
+  type ButtonButtonProps,
+  type ButtonProps,
+} from './components/Button/Button'
+
+export { default as Spinner } from './components/Spinner/Spinner'
